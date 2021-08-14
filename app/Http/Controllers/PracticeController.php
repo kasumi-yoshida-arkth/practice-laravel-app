cd78680ae974f5a5f025bb9e2905d8b337d9b0ae
su-min484 <85155408+su-min484@users.noreply.github.com>
2021-08-15 02:34:26 +0900

    冗長な記述修正

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -10,9 +10,7 @@ class PracticeController extends Controller
     public function index()
     {
         // bladeに渡すデータを以下に記載
-        $data =array();
-        $data['name'] = 'Kasumi';
-        $data['message'] = 'Hey';
+        $data = ['name'=>'Kasumi', 'message'=>'Hey'];
         // practice.blade.phpを返している
         return view('practice')->with(
             'data', $data
